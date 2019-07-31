0df6e0b0ce4814da9fdef0bdf8c444610e1b1374
Aram Sabatés <asbermudez@gmail.com>
2019-07-31 17:15:59 +0200

    Added new tests

---
+++ tests/unit/example.spec.ts
@@ -0,0 +1,87 @@
+import xStateMixin from '../../src/index';
+import { MachineConfig, MachineOptions, interpret } from 'xstate';
+import { Subject } from 'rxjs';
+
+jest.mock('xstate', () => ({
+    Machine: jest.fn().mockImplementation((a: any, b: any, initialContext: any) => ({
+        withConfig: jest.fn().mockReturnValue({
+            context: initialContext,
+            initialState: { value: 'mockState' }
+        })
+    })),
+    interpret: jest.fn().mockReturnValue({
+        onChange: jest.fn(),
+        onTransition: jest.fn(),
+        send: jest.fn(),
+        start: jest.fn()
+    })
+}));
+
+describe('xStateMixin', () => {
+    interface MockEvent { type: 'mock'; }
+    const mockEvent: MockEvent = { type: 'mock' };
+    let mixin: xStateMixin<{}, {}, MockEvent>;
+
+    describe('xStateInit', () => {
+        beforeEach(() => {
+            mixin = new xStateMixin();
+        });
+
+        it('should set the machine and initialize state and hash', () => {
+            // GIVEN
+            const initialHash = mixin.stateHash;
+            const mockInitialContext = {};
+            const mockConfig = { context: mockInitialContext } as MachineConfig<{}, {}, MockEvent>;
+            const mockOptions = {} as MachineOptions<{}, MockEvent>;
+
+            // WHEN
+            mixin.xStateInit(mockInitialContext, mockConfig, mockOptions);
+
+            // THEN
+            expect(mixin.context).toBe(mockInitialContext);
+            expect(mixin.stateHash).not.toBe(initialHash);
+            expect(mixin.currentState).toBe('mockState');
+            expect(interpret({} as any).onChange).toHaveBeenCalled();
+            expect(interpret({} as any).onTransition).toHaveBeenCalled();
+        });
+
+        it('should set the subscription if channel is set in prop', () => {
+            // GIVEN
+            const mockChannel = new Subject<MockEvent>();
+            const subscribeSpy = jest.spyOn(mockChannel, 'subscribe');
+            mixin.channel = mockChannel;
+            const mockInitialContext = {};
+            const mockConfig = { context: mockInitialContext } as MachineConfig<{}, {}, MockEvent>;
+            const mockOptions = {} as MachineOptions<{}, MockEvent>;
+
+            // WHEN
+            mixin.xStateInit(mockInitialContext, mockConfig, mockOptions);
+            mockChannel.next(mockEvent);
+
+            // THEN
+            expect(subscribeSpy).toHaveBeenCalled();
+            expect(interpret({} as any).send).toHaveBeenCalledWith(mockEvent);
+        });
+    });
+
+    describe('dispatch', () => {
+        beforeEach(() => {
+            mixin = new xStateMixin();
+            const mockInitialContext = {};
+            const mockConfig = { context: mockInitialContext } as MachineConfig<{}, {}, MockEvent>;
+            const mockOptions = {} as MachineOptions<{}, MockEvent>;
+            mixin.xStateInit(mockInitialContext, mockConfig, mockOptions);
+        });
+
+        it('should trigger interpreter send when called', () => {
+            // GIVEN
+            const sendSpy = interpret({} as any).send;
+
+            // WHEN
+            mixin.dispatch(mockEvent);
+
+            // THEN
+            expect(sendSpy).toHaveBeenCalledWith(mockEvent);
+        });
+    });
+});
